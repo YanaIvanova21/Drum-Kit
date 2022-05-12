d77bcb1f51136b3a3e8657322c04c2715a8ad937
YanaIvanova21 <yana.ivanova.199621@gmail.com>
2022-05-12 17:26:45 +0300

    Audio added to the keyboard keys

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfDrumButtons; i++) {
    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
    var buttonInnerHTML = this.innerHTML;
-   switch (buttonInnerHTML){
+   makeSound(buttonInnerHTML);
+       }); 
+}      
+       
+//Detecting Keyboard Press
+
+document.addEventListener("keydown", function(event) {
+     makeSound(event.key);
+});
+
+function makeSound(key) {
+
+switch (key){
        case "w": 
             let tom1 = new Audio('sounds/tom-1.mp3');
             tom1.play(); 
@@ -40,9 +52,9 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
        break;
 
        default: console.log("this.innerHTML");
-   }
-       });   
+   };
 }
+
 
 
 
